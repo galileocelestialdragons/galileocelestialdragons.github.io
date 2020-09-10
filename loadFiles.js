116cb3f7e319cd6e0177c2a9ac3c761c86f1d5c9
Glitch (jasper-shocking-antimony) <none>
2020-09-10 23:28:49 +0000

    🎳⛲️ Updated FAQ with Glitch

--- loadFiles.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-var xhr = new XMLHttpRequest;
-xhr.open('GET', 'test.txt', true);
-xhr.onload = function (){
-    document.getElementById('test').innerHTML = xhr.responseText;
-};
-xhr.send(null);
-
-}
